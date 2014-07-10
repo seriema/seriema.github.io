c1fb7c40d27ffb6b50813cb7451b96340dc60a2e
John-Philip Johansson <seriema@gmail.com>
2014-07-10 18:50:58 -0300

    Auto-commit. Check "dev" branch for source.

--- scripts/3966f6a4.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("seriemagithubioApp",[]),angular.module("seriemagithubioApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
